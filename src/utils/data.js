4341d7f4b90488bffb8fc37e1043d668e6091604
Dmitrii Koshelev <me@koshelev.net>
2021-09-24 14:43:09 +0300

    Remove unnecessary data

--- src/utils/data.js
+++ src/utils/data.js
@@ -4,19 +4,4 @@ export const INGREDIENTS_TYPES = [
   { slug: 'main', title: 'Начинки' },
 ];
 
-export const BASE_BUN = {
-  _id: '60d3b41abdacab0026a733c6',
-  name: 'Краторная булка N-200i',
-  type: 'bun',
-  proteins: 80,
-  fat: 24,
-  carbohydrates: 53,
-  calories: 420,
-  price: 20,
-  image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-  image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-  image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-  __v: 0,
-};
-
-export const DEFAULT_ORDER_ID = '034536';
+export const DEFAULT_ORDER_ID = 0;

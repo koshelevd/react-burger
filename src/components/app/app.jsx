c93f34cf2bb11111f4aac65339ef0bf4736f4bc5
Dmitrii Koshelev <me@koshelev.net>
2021-09-24 14:42:31 +0300

    Refactor to use context

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,45 +3,27 @@ import AppHeader from '../app-header/app-header';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients.jsx';
 import BurgerConstructor from '../burger-constructor/burger-constructor.jsx';
 import styles from './app.module.css';
-import { INGREDIENTS_TYPES } from '../../utils/data';
 import api from '../../utils/api';
 import { IngredientsContext } from '../../contexts/ingredients-context';
 
 function App() {
-  console.log('App render');
-  const ingredientsState = useState({
-    all: [],
-    buns: [],
-    types: INGREDIENTS_TYPES,
-  });
-  const [ingredients, setIngredients] = ingredientsState;
-  const [rawData, setRawData] = useState([]);
+  const [ingredients, setIngredients] = useState([]);
 
   useEffect(() => {
     api
       .getIngredients()
       .then((result) => {
-        setRawData(result.data);
+        setIngredients(result.data);
       })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  useEffect(() => {
-    setIngredients({
-      ...ingredients,
-      all: rawData,
-      buns: [], // rawData.filter((i) => i.type !== 'bun'),
-    });
-  }, [rawData, ingredients]);
+      .catch((err) => console.log(err));
+  }, [setIngredients]);
 
   return (
-    <IngredientsContext.Provider value={ingredientsState}>
+    <IngredientsContext.Provider value={[ingredients, setIngredients]}>
       <AppHeader />
       <main className={`${styles.main} pl-5 pr-5`}>
         <BurgerIngredients />
-        {/* <BurgerConstructor /> */}
+        <BurgerConstructor />
       </main>
     </IngredientsContext.Provider>
   );

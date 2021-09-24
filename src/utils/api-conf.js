ba4212d3a00b485bfddbb2860fb7027a5627122e
Dmitrii Koshelev <me@koshelev.net>
2021-09-24 14:43:40 +0300

    Add /checkout/ endpoint to api actions

--- src/utils/api-conf.js
+++ src/utils/api-conf.js
@@ -1,3 +1,4 @@
 export const API_URL = 'https://norma.nomoreparties.space/api';
 export const INGREDIENTS_ENDPOINT = '/ingredients';
+export const CHECKOUT_ENDPOINT = '/orders';
 export const ERROR_TEXT = 'Error';
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,15 @@
-import { API_URL, INGREDIENTS_ENDPOINT, ERROR_TEXT } from './api-conf';
+import {
+  API_URL,
+  INGREDIENTS_ENDPOINT,
+  CHECKOUT_ENDPOINT,
+  ERROR_TEXT,
+} from './api-conf';
 
 class Api {
-  constructor(baseUrl, endpoint) {
+  constructor(baseUrl, getIngredientsEndpoint, checkoutEndpoint) {
     // Initialize object.
-    this.url = baseUrl + endpoint;
+    this.getIngredientsUrl = baseUrl + getIngredientsEndpoint;
+    this.checkoutUrl = baseUrl + checkoutEndpoint;
     this.headers = {
       Accept: 'application/json',
       'Content-Type': 'application/json',
@@ -20,11 +26,20 @@ class Api {
 
   getIngredients() {
     // Get all ingredients data.
-    return fetch(this.url, {
+    return fetch(this.getIngredientsUrl, {
       headers: this.headers,
+    }).then(this._processResponse);
+  }
+
+  checkout(data) {
+    // Process checkout: post ingredients.
+    return fetch(this.checkoutUrl, {
+      method: 'POST',
+      headers: this.headers,
+      body: JSON.stringify(data),
     }).then(this._processResponse);
   }
 }
 
-const api = new Api(API_URL, INGREDIENTS_ENDPOINT);
+const api = new Api(API_URL, INGREDIENTS_ENDPOINT, CHECKOUT_ENDPOINT);
 export default api;

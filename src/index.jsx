e9330191b881fbcc19f220647259e522f6f2fd4b
Dmitrii Koshelev <me@koshelev.net>
2021-11-13 22:35:38 +0300

    Add BrowserRouter to application

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/app/app';
 import { store } from './services/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );

d149b491fcf082503f8f2522d909fbfbdf343c96
Koshelev Dmitrii <me@koshelev.net>
2021-11-14 22:00:49 +0300

    Add index file for components

---
+++ src/components/index.jsx
@@ -0,0 +1,6 @@
+export { default as App } from './app/app';
+export { default as BurgerConstructor } from './burger-constructor/burger-constructor';
+export { default as BurgerIngredients } from './burger-ingredients/burger-ingredients';
+export { default as IngredientDetails } from './ingredient-details/ingredient-details';
+export { default as Modal } from './modal/modal';
+export { default as OrderDetails } from './order-details/order-details';

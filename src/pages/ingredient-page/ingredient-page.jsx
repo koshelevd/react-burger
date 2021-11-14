6b04c90ea28c30ca75f4559be3269f6d96dd837a
Koshelev Dmitrii <me@koshelev.net>
2021-11-14 21:59:38 +0300

    Develop ingredient detail page

---
+++ src/pages/ingredient-page/ingredient-page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './ingredient-page.module.css';
+
+function IngredientPage() {
+  return (
+    <div className={styles.wrapper}>
+      <form className={styles.form}>
+        <h1 className={styles.heading}>Ingredient</h1>
+      </form>
+    </div>
+  );
+}
+export default IngredientPage;

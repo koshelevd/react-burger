ea60b86f36f58de45d5774bdf09df7697619ee4c
Koshelev Dmitrii <me@koshelev.net>
2021-11-14 22:00:22 +0300

    Add index file for pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export { default as MainPage } from './main-page/main-page';
+export { default as LoginPage } from './login-page/login-page';
+export { default as RegisterPage } from './register-page/register-page';
+export { default as ForgotPasswordPage } from './forgot-password-page/forgot-password-page';
+export { default as ResetPasswordPage } from './reset-password-page/reset-password-page';
+export { default as ProfilePage } from './profile-page/profile-page';
+export { default as IngredientPage } from './ingredient-page/ingredient-page';
+export { default as NotFoundPage } from './not-found-page/not-found-page';

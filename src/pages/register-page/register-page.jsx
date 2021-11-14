fcd09744dbd2780d9b125bbc3caecdeda266fc42
Koshelev Dmitrii <me@koshelev.net>
2021-11-14 21:56:42 +0300

    Develop sign up page

--- src/pages/register-page/register-page.jsx
+++ src/pages/register-page/register-page.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  EmailInput,
+  Input,
+  PasswordInput,
+  Button,
+} from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './register-page.module.css';
 
 function RegisterPage() {
   return (
-    <div className={styles.wrapper}>
-      <form className={styles.form}>
-        <h1 className={styles.heading}>Регистрация</h1>
+    <main className={`${styles.main}`}>
+      <h1 className={`${styles.heading} text text_type_main-medium pb-6`}>
+        Регистрация
+      </h1>
+      <form className={`${styles.form} mb-20`}>
+        <div className="mb-6">
+          <Input placeholder="Имя" />
+        </div>
+        <div className="mb-6">
+          <EmailInput />
+        </div>
+        <div className="mb-6">
+          <PasswordInput />
+        </div>
+        <Button>Зарегистрироваться</Button>
       </form>
-    </div>
+
+      <p
+        className={`${styles.info} text text_type_main-default text_color_inactive mb-4`}
+      >
+        Уже зарегистрированы?{' '}
+        <Link to="/login" className="link">
+          Войти
+        </Link>
+      </p>
+    </main>
   );
 }
 export default RegisterPage;

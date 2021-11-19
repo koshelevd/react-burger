ef1f74a4a36e0f53b0773320f49413ccef55cd57
Dmitrii Koshelev <me@koshelev.net>
2021-11-19 23:32:39 +0300

    Develop profile page

--- src/pages/profile-page/profile-page.jsx
+++ src/pages/profile-page/profile-page.jsx
@@ -1,11 +1,104 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  // PasswordInput,
+  Input,
+  Button,
+} from '@ya.praktikum/react-developer-burger-ui-components';
+import { ProfileNavigation } from '../../components';
+import { getProfile, patchProfile } from '../../services/slices/auth-slice';
+import { useFormWithValidation } from '../../hooks/useFormWithValidation';
 import styles from './profile-page.module.css';
 
 function ProfilePage() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((store) => store.auth);
+  const { values, handleChange, errors, resetForm } = useFormWithValidation();
+
+  const [isEditName, setIsEditName] = useState(false);
+  const [isEditEmail, setIsEditEmail] = useState(false);
+  const [isEditPassword, setIsEditPassword] = useState(false);
+
+  function onEditEnd() {
+    // resetForm(user);
+    setIsEditName(false);
+    setIsEditEmail(false);
+    setIsEditPassword(false);
+  }
+
+  function handleCancel(e) {
+    e.preventDefault();
+    onEditEnd();
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(patchProfile(values));
+    onEditEnd();
+  }
+
+  useEffect(() => {
+    if (!user) {
+      dispatch(getProfile());
+    } else {
+      resetForm(user);
+    }
+  }, [user]);
+
   return (
     <div className={styles.wrapper}>
+      <aside className={styles.sidebar}>
+        <ProfileNavigation />
+      </aside>
       <form className={styles.form}>
-        <h1 className={styles.heading}>Profile</h1>
+        <div className="mb-6">
+          <Input
+            placeholder="Имя"
+            icon="EditIcon"
+            value={values.name}
+            name="name"
+            error={!!errors.name}
+            errorText={errors.name}
+            onChange={handleChange}
+            onIconClick={() => setIsEditName(!isEditName)}
+            disabled={!isEditName}
+          />
+        </div>
+        <div className="mb-6">
+          <Input
+            type="email"
+            placeholder="Логин"
+            icon="EditIcon"
+            value={values.email}
+            name="email"
+            error={!!errors.email}
+            errorText={errors.email}
+            onChange={handleChange}
+            onIconClick={() => setIsEditEmail(!isEditEmail)}
+            disabled={!isEditEmail}
+          />
+        </div>
+        <div className="mb-6">
+          <Input
+            placeholder="Пароль"
+            icon="EditIcon"
+            value={values.password}
+            name="password"
+            error={!!errors.password}
+            errorText={errors.password}
+            onChange={handleChange}
+            onIconClick={() => setIsEditPassword(!isEditPassword)}
+            disabled={!isEditPassword}
+          />
+        </div>
+        {(isEditName || isEditEmail || isEditPassword) && (
+          <div className={styles.buttons}>
+            <Button onClick={handleSubmit}>Сохранить</Button>
+            <Button onClick={handleCancel} type="secondary">
+              Отмена
+            </Button>
+          </div>
+        )}
       </form>
     </div>
   );

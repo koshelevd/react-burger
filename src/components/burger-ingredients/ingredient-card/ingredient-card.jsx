749ed7dd89834eb4904ee0a6086b30421357f557
Dmitrii Koshelev <me@koshelev.net>
2021-11-19 23:23:58 +0300

    Change logic to use ingredients with own pages

--- src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
+++ src/components/burger-ingredients/ingredient-card/ingredient-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {
   Counter,
@@ -9,6 +10,7 @@ import styles from './ingredient-card.module.css';
 import { useDrag } from 'react-dnd';
 
 const IngredientCard = React.memo(({ data }) => {
+  const location = useLocation();
   const { _id } = data;
   const [, dragRef] = useDrag({
     type: 'ingredient',
@@ -16,19 +18,28 @@ const IngredientCard = React.memo(({ data }) => {
   });
 
   return (
-    <figure className={styles.figure} ref={dragRef}>
-      <img src={data.image} alt={data.name} className={styles.picture} />
-      {!!data.count && <Counter count={data.count} size="default" />}
-      <p className={`${styles.price} mt-2 mb-2`}>
-        <span className="text text_type_digits-default">
-          {data.price}&nbsp;
-        </span>
-        <CurrencyIcon type="primary" />
-      </p>
-      <figcaption className={`text text_type_main-default ${styles.caption}`}>
-        {data.name}
-      </figcaption>
-    </figure>
+    <Link
+      to={`/ingredients/${_id}`}
+      state={{ backgroundLocation: location }}
+      className={styles.link}
+      ref={dragRef}
+    >
+      <figure className={styles.figure}>
+        <img src={data.image} alt={data.name} className={styles.picture} />
+        {!!data.count && <Counter count={data.count} size="default" />}
+        <p className={`${styles.price} mt-2 mb-2`}>
+          <span className="text text_type_digits-default text_color_primary">
+            {data.price}&nbsp;
+          </span>
+          <CurrencyIcon type="primary" />
+        </p>
+        <figcaption
+          className={`text text_type_main-default text_color_primary ${styles.caption}`}
+        >
+          {data.name}
+        </figcaption>
+      </figure>
+    </Link>
   );
 });
 

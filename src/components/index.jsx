300c6f3651067a8cfc316535ce6469e53d99ebf9
Dmitrii Koshelev <me@koshelev.net>
2021-11-19 22:53:56 +0300

    Add index file for components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,3 +4,7 @@ export { default as BurgerIngredients } from './burger-ingredients/burger-ingred
 export { default as IngredientDetails } from './ingredient-details/ingredient-details';
 export { default as Modal } from './modal/modal';
 export { default as OrderDetails } from './order-details/order-details';
+export { default as AuthForm } from './auth-form/auth-form';
+export { default as ProfileNavigation } from './profile-navigation/profile-navigation';
+export { default as RequireAuth } from './require-auth/require-auth';
+export { default as Layout } from './layout/layout';

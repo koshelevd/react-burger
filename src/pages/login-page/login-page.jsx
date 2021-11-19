062288ece72f59cd6eccddaecdb1f9e96e57ef71
Dmitrii Koshelev <me@koshelev.net>
2021-11-19 23:32:17 +0300

    Develop login page

--- src/pages/login-page/login-page.jsx
+++ src/pages/login-page/login-page.jsx
@@ -1,45 +1,76 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, Navigate } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
 import {
-  EmailInput,
+  Input,
   PasswordInput,
-  Button,
 } from '@ya.praktikum/react-developer-burger-ui-components';
-import styles from './login-page.module.css';
+import { AuthForm } from '../../components';
+import authSlice, { signIn } from '../../services/slices/auth-slice';
+import { useFormWithValidation } from '../../hooks/useFormWithValidation';
 
 function LoginPage() {
-  return (
-    <main className={`${styles.main}`}>
-      <h1 className={`${styles.heading} text text_type_main-medium pb-6`}>
-        Вход
-      </h1>
-      <form className={`${styles.form} mb-20`}>
-        <div className="mb-6">
-          <EmailInput />
-        </div>
-        <div className="mb-6">
-          <PasswordInput />
-        </div>
-        <Button>Войти</Button>
-      </form>
+  const dispatch = useDispatch();
+  let location = useLocation();
+  const { clearError } = authSlice.actions;
+  const { isLoggedIn, error } = useSelector((store) => store.auth);
+  const { values, handleChange, errors, isValid } = useFormWithValidation();
+  const formInfo = [
+    {
+      text: 'Вы — новый пользователь?',
+      linkTitle: 'Зарегистрироваться',
+      linkUrl: '/register',
+    },
+    {
+      text: 'Забыли пароль?',
+      linkTitle: 'Восстановить пароль',
+      linkUrl: '/forgot-password',
+    },
+  ];
 
-      <p
-        className={`${styles.info} text text_type_main-default text_color_inactive mb-4`}
-      >
-        Вы — новый пользователь?{' '}
-        <Link to="/register" className="link">
-          Зарегистрироваться
-        </Link>
-      </p>
-      <p
-        className={`${styles.info} text text_type_main-default text_color_inactive mb-4`}
-      >
-        Забыли пароль?{' '}
-        <Link to="/forgot-password" className="link">
-          Восстановить пароль
-        </Link>
-      </p>
-    </main>
+  useEffect(() => {
+    dispatch(clearError());
+  }, [dispatch, clearError]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(signIn(values));
+  }
+
+  if (isLoggedIn) return <Navigate to="/" state={{ from: location }} />;
+
+  return (
+    <AuthForm
+      header="Вход"
+      buttonText="Войти"
+      formInfo={formInfo}
+      onSubmit={handleSubmit}
+      isValid={isValid}
+      error={error}
+    >
+      <div className="mb-6">
+        <Input
+          type="email"
+          placeholder="E-mail"
+          value={values.email}
+          name="email"
+          error={!!errors.email}
+          errorText={errors.email}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <PasswordInput
+          placeholder="Пароль"
+          value={values.password}
+          name="password"
+          error={!!errors.password}
+          errorText={errors.password}
+          onChange={handleChange}
+        />
+      </div>
+    </AuthForm>
   );
 }
 export default LoginPage;

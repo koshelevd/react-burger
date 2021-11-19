53463da973abf8ff04ed68fb734ed7e93e3e9cb0
Dmitrii Koshelev <me@koshelev.net>
2021-11-19 23:18:15 +0300

    Develop container for authentication forms

---
+++ src/components/auth-form/auth-form.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './auth-form.module.css';
+
+function AuthForm({ header, children, buttonText, formInfo, onSubmit, isValid, error }) {
+  return (
+    <main className={`${styles.main}`}>
+      <h1 className={`${styles.heading} text text_type_main-medium pb-6`}>
+        {header}
+      </h1>
+      <form className={`${styles.form} mb-20`} onSubmit={onSubmit}>
+        {!!error && <p>{error}</p>}
+        {children}
+        <Button disabled={!isValid}>{buttonText}</Button>
+      </form>
+
+      {formInfo &&
+        formInfo.map((data) => (
+          <p
+            className={`${styles.info} text text_type_main-default text_color_inactive mb-4`}
+          >
+            {data.text}{' '}
+            <Link to={data.linkUrl} className="link">
+              {data.linkTitle}
+            </Link>
+          </p>
+        ))}
+    </main>
+  );
+}
+export default AuthForm;

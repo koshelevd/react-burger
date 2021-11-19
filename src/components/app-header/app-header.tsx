65aa10e1e8cd8f54bcfb79973434d75d8f72c7a2
Dmitrii Koshelev <me@koshelev.net>
2021-11-19 22:57:17 +0300

    Refactor app-header component to use routes and navigation

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,44 +1,93 @@
-import React from 'react';
-import styles from './app-header.module.css';
+import { NavLink, useMatch } from 'react-router-dom';
 import {
   BurgerIcon,
   Logo,
   ListIcon,
   ProfileIcon,
 } from '@ya.praktikum/react-developer-burger-ui-components';
+import styles from './app-header.module.css';
 
 function AppHeader() {
+  const isMainPage = useMatch('/');
+  const isFeedPage = useMatch('/feed');
+  const isProfilePage = useMatch('/profile/*');
+
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
         <ul className={styles.menu}>
           <li className={`${styles.item} pl-5 pr-5 mr-2`}>
-            <span className="mr-2">
-              <BurgerIcon type="primary" />
-            </span>
-            <span className="text text_type_main-default">Конструктор</span>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `${
+                  styles.link
+                } text text_type_main-default text_color_inactive ${
+                  isActive && styles.active
+                }`
+              }
+            >
+              <span className="mr-2">
+                <BurgerIcon type={isMainPage ? 'primary' : 'secondary'} />
+              </span>
+              <span className={styles.text}>Конструктор</span>
+            </NavLink>
           </li>
-          <li className={`${styles.item} pl-5 pr-5`}>
+          <li className={`${styles.item} pl-5 pr-5 mr-2`}>
+            <NavLink
+              to="/feed"
+              className={({ isActive }) =>
+                `${
+                  styles.link
+                } text text_type_main-default text_color_inactive ${
+                  isActive && styles.active
+                }`
+              }
+            >
+              <span className="mr-2">
+                <ListIcon type={isFeedPage ? 'primary' : 'secondary'} />
+              </span>
+              <span className={styles.text}>Лента заказов</span>
+            </NavLink>
+          </li>
+          {/* <li className={`${styles.item} pl-5 pr-5`}>
             <span className="mr-2">
               <ListIcon type="secondary" />
             </span>
             <span className="text text_type_main-default text_color_inactive">
               Лента заказов
             </span>
-          </li>
+          </li> */}
         </ul>
         <span className={styles.logo}>
           <Logo />
         </span>
         <ul className={`${styles.menu} ${styles.menu_right}`}>
-          <li className={`${styles.item} pl-5 pr-5`}>
+        <li className={`${styles.item} pl-5 pr-5 mr-2`}>
+            <NavLink
+              to="/profile"
+              className={({ isActive }) =>
+                `${
+                  styles.link
+                } text text_type_main-default text_color_inactive ${
+                  isActive && styles.active
+                }`
+              }
+            >
+              <span className="mr-2">
+                <ProfileIcon type={isProfilePage ? 'primary' : 'secondary'} />
+              </span>
+              <span className={styles.text}>Личный кабинет</span>
+            </NavLink>
+          </li>
+          {/* <li className={`${styles.item} pl-5 pr-5`}>
             <span className="mr-2">
               <ProfileIcon type="secondary" />
             </span>
             <span className="text text_type_main-default text_color_inactive">
               Личный кабинет
             </span>
-          </li>
+          </li> */}
         </ul>
       </nav>
     </header>

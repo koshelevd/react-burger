b241405283fe11fd7089e516cd50540a8e8175f5
Koshelev Dmitrii <me@koshelev.net>
2021-11-20 16:51:41 +0300

    Refactor to use ProfileLayout

--- src/pages/profile-page/profile-page.jsx
+++ src/pages/profile-page/profile-page.jsx
@@ -5,7 +5,6 @@ import {
   Input,
   Button,
 } from '@ya.praktikum/react-developer-burger-ui-components';
-import { ProfileNavigation } from '../../components';
 import { getProfile, patchProfile } from '../../services/slices/auth-slice';
 import { useFormWithValidation } from '../../hooks/useFormWithValidation';
 import styles from './profile-page.module.css';
@@ -43,64 +42,59 @@ function ProfilePage() {
     } else {
       resetForm(user);
     }
-  }, [user]);
+  }, [user, dispatch, resetForm]);
 
   return (
-    <div className={styles.wrapper}>
-      <aside className={styles.sidebar}>
-        <ProfileNavigation />
-      </aside>
-      <form className={styles.form}>
-        <div className="mb-6">
-          <Input
-            placeholder="Имя"
-            icon="EditIcon"
-            value={values.name}
-            name="name"
-            error={!!errors.name}
-            errorText={errors.name}
-            onChange={handleChange}
-            onIconClick={() => setIsEditName(!isEditName)}
-            disabled={!isEditName}
-          />
+    <form className={styles.form}>
+      <div className="mb-6">
+        <Input
+          placeholder="Имя"
+          icon="EditIcon"
+          value={values.name}
+          name="name"
+          error={!!errors.name}
+          errorText={errors.name}
+          onChange={handleChange}
+          onIconClick={() => setIsEditName(!isEditName)}
+          disabled={!isEditName}
+        />
+      </div>
+      <div className="mb-6">
+        <Input
+          type="email"
+          placeholder="Логин"
+          icon="EditIcon"
+          value={values.email}
+          name="email"
+          error={!!errors.email}
+          errorText={errors.email}
+          onChange={handleChange}
+          onIconClick={() => setIsEditEmail(!isEditEmail)}
+          disabled={!isEditEmail}
+        />
+      </div>
+      <div className="mb-6">
+        <Input
+          placeholder="Пароль"
+          icon="EditIcon"
+          value={values.password}
+          name="password"
+          error={!!errors.password}
+          errorText={errors.password}
+          onChange={handleChange}
+          onIconClick={() => setIsEditPassword(!isEditPassword)}
+          disabled={!isEditPassword}
+        />
+      </div>
+      {(isEditName || isEditEmail || isEditPassword) && (
+        <div className={styles.buttons}>
+          <Button onClick={handleSubmit}>Сохранить</Button>
+          <Button onClick={handleCancel} type="secondary">
+            Отмена
+          </Button>
         </div>
-        <div className="mb-6">
-          <Input
-            type="email"
-            placeholder="Логин"
-            icon="EditIcon"
-            value={values.email}
-            name="email"
-            error={!!errors.email}
-            errorText={errors.email}
-            onChange={handleChange}
-            onIconClick={() => setIsEditEmail(!isEditEmail)}
-            disabled={!isEditEmail}
-          />
-        </div>
-        <div className="mb-6">
-          <Input
-            placeholder="Пароль"
-            icon="EditIcon"
-            value={values.password}
-            name="password"
-            error={!!errors.password}
-            errorText={errors.password}
-            onChange={handleChange}
-            onIconClick={() => setIsEditPassword(!isEditPassword)}
-            disabled={!isEditPassword}
-          />
-        </div>
-        {(isEditName || isEditEmail || isEditPassword) && (
-          <div className={styles.buttons}>
-            <Button onClick={handleSubmit}>Сохранить</Button>
-            <Button onClick={handleCancel} type="secondary">
-              Отмена
-            </Button>
-          </div>
-        )}
-      </form>
-    </div>
+      )}
+    </form>
   );
 }
 export default ProfilePage;

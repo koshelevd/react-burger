51d1e9aa4d25075a0df50c6fb8dbb9bae5a4d5f9
Koshelev Dmitrii <me@koshelev.net>
2021-11-20 16:53:15 +0300

    Rearrange info on page

--- src/pages/not-found-page/not-found-page.jsx
+++ src/pages/not-found-page/not-found-page.jsx
@@ -3,11 +3,9 @@ import styles from './not-found-page.module.css';
 
 function NotFoundPage() {
   return (
-    <div className={styles.wrapper}>
-      <form className={styles.form}>
-        <h1 className={styles.heading}>404</h1>
-      </form>
-    </div>
+    <main className={styles.main}>
+      <h1 className={styles.heading}>Page not found</h1>
+    </main>
   );
 }
 

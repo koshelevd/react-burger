9830f57adfb69a11152aa4f0d990143cf07f18fb
Dmitrii Koshelev <me@koshelev.net>
2021-12-08 12:47:32 +0300

    Convert utils to Typescript

--- src/utils/data.js
+++
@@ -1,7 +0,0 @@
-export const INGREDIENTS_TYPES = [
-  { slug: 'bun', title: 'Булки' },
-  { slug: 'sauce', title: 'Соусы' },
-  { slug: 'main', title: 'Начинки' },
-];
-
-export const DEFAULT_ORDER_ID = 0;
---
+++ src/utils/data.ts
@@ -0,0 +1,9 @@
+import { TIngredientType } from "./types";
+
+export const INGREDIENTS_TYPES: Array<TIngredientType> = [
+  { slug: 'bun', title: 'Булки' },
+  { slug: 'sauce', title: 'Соусы' },
+  { slug: 'main', title: 'Начинки' },
+];
+
+export const DEFAULT_ORDER_ID: number = 0;
